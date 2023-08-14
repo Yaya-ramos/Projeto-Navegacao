a170e2d813d5b4943f123ac2d09937bff7e91670
Yaya-ramos <129106352+Yaya-ramos@users.noreply.github.com>
2023-08-14 12:12:34 -0300

    Atividade do dia 14/08
    Yara Ramos Dias 3°TII/G1

---
+++ lib/telas/tela_categoria.dart
@@ -0,0 +1,32 @@
+
+import 'package:flutter/material.dart';
+import 'package:projeto2/componentes/item_categoria.dart';
+import 'package:projeto2/data/mock_data.dart';
+
+
+
+class TelaCategoria extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cardápio"),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200, //Tamanho de cada elemento
+          childAspectRatio: 3/2, //proporçao do elemento na tela
+          crossAxisSpacing: 20, //espaçamento entre os eixos
+          mainAxisSpacing: 20, //espaçamento da tela
+          ),
+          children: mockCategories.map(
+            (categoria) {
+              return ItemCategoria(categoria);
+            }
+            ).toList(),
+          ),
+        );
+  }
+}
